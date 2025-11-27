35e5ca1616b027ec3fce29c3860035fea8f97411
hiromu terashima <hiromu.terashima@epkotsoftware.co.jp>
2025-11-28 01:16:37 +0900

    CRUD機能作成

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,17 +4,37 @@ class TasksController < ApplicationController
   end
 
   def new
+    @task = Task.new
+  end
+
+  def create
+    Task.create(task_params)
+    redirect_to tasks_path
   end
 
   def show
+    @task = Task.find(params[:id])
   end
 
   def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    task = Task.find(params[:id])
+    task.update(task_params)
+    redirect_to tasks_path
   end
 
   def destroy
     task = Task.find(params[:id])
     task.destroy
-    redirect_to tasks_path,
+    redirect_to tasks_path
   end
+
+  private
+
+def task_params
+  params.require(:task).permit(:title, :description)
+end
 end
