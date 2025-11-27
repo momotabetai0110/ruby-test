f4068843542791f6d0774f546894d79891f3fcd5
hiromu terashima <hiromu.terashima@epkotsoftware.co.jp>
2025-11-27 18:37:58 +0900

    DB・テストデータ作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,17 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+#本来seeds.rb は「何度実行しても重複レコードが増えない」ことが推奨されている
+Task.create!(
+    title: "買い物リスト作成",
+    description: "週末の食材を整理する"
+  )
+  Task.create!(
+    title: "レポート提出",
+    description: "月次レポートのドラフトを金曜までに",
+  )
+  Task.create!(
+    title:"企業面接",
+    description:nil
+  )
