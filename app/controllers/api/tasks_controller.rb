83a3ca14d68db521d77bdef6bd126a505c7e634b
hiromu terashima <hiromu.terashima@epkotsoftware.co.jp>
2025-11-30 15:00:29 +0900

    バリデーションチェック追加

--- app/controllers/api/tasks_controller.rb
+++ app/controllers/api/tasks_controller.rb
@@ -2,9 +2,8 @@ class Api::TasksController < ApplicationController
   protect_from_forgery with: :null_session
 
   # タスクを全件取得する
-  # タスクが存在しない場合はメッセージを返す
+  # タスクが存在しない場合は存在しない旨をメッセージを返す
   # @return [Array<Task>] タスク情報一覧
-  # @return [Hash] タスクが存在しない場合 { status: "OK", result: "タスクはありません" }
   def index
     tasks = Task.all
     if tasks.empty?
@@ -15,9 +14,8 @@ class Api::TasksController < ApplicationController
   end
 
   # IDを指定してタスクを取得する
-  # そのIDに対応するタスクが存在しない場合はメッセージを返す
+  # そのIDに対応するタスクが存在しない場合は存在しない旨をメッセージを返す
   # @return [Task] タスク情報
-  # @return [Hash] タスクが存在しない場合 { status: "OK", result: "{ID}そのタスクはありません" }
   def show
     task = Task.find_by(id: params[:id])
     id =  params[:id]
@@ -31,31 +29,38 @@ class Api::TasksController < ApplicationController
   # 受け取った値から新規タスクを作成する
   # @param task_params [ActionController::Parameters] タスク名、説明、期日を含んだパラメータ
   # @return [Hash] 作成したタスク情報
+  # @return [Hash] バリデーションエラー:エラーメッセージ
   def create
     task = Task.create(task_params)
-    render json: { status: "OK", result: task }, status: :ok
+    if task.persisted?
+      render json: { status: "OK", result: task }, status: :created
+    else
+      render json: { status: "NG", errors: task.errors.full_messages }, status: :unprocessable_entity
+    end
   end
 
   # IDで指定したタスクの情報を更新する
-  # そのIDに対応するタスクが存在しない場合はメッセージを返す
+  # そのIDに対応するタスクが存在しない場合は存在しない旨をメッセージを返す
   # @param task_params [ActionController::Parameters] タスク名、説明、期日を含んだパラメータ
   # @return [Hash] 更新したタスク情報
-  # @return [Hash] タスクが存在しない場合 { status: "OK", result: "{ID}更新するタスクはありません" }
+  # @return [Hash] バリデーションエラー:エラーメッセージ
   def update
     task = Task.find_by(id: params[:id])
     if task.nil?
       render json: { status: "OK", result: "[ID:#{params[:id]}]更新するタスクが見つかりません" }
       return
     end
-    task.update(task_params)
-    render json: { status: "OK", result: task }, status: :ok
+
+    if task.update(task_params)
+      render json: { status: "OK", result: task }, status: :ok
+    else
+      render json: { status: "NG", errors: task.errors.full_messages }, status: :unprocessable_entity
+    end
   end
 
   # IDで指定したタスクを削除する
-  # そのIDに対応するタスクが存在しない場合はメッセージを返す
-  # @param task_params [ActionController::Parameters] タスク名、説明、期日を含んだパラメータ
+  # そのIDに対応するタスクが存在しない場合は存在しない旨をメッセージを返す
   # @return [Hash] 削除したタスク情報 { status: "OK", result: Task }
-  # @return [Hash] タスクが存在しない場合 { status: "OK", result: "{ID}削除するタスクはありません" }
   def destroy
     task = Task.find_by(id: params[:id])
     if task.nil?
@@ -67,8 +72,8 @@ class Api::TasksController < ApplicationController
   end
 
   # 最も新しいタスクを削除する
+  # タスクが存在しない場合は存在しない旨をメッセージを返す
   # @return [Hash] 削除したタスク情報 { status: "OK", result: Task }
-  # @return [Hash] タスクが存在しない場合 { status: "OK", result: "タスクが1つもありません" }
   def delete_recently_task
     task = Task.order(created_at: :desc).first
     if task.nil?
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,11 @@
 class Task < ApplicationRecord
+# validates :カラム名,バリデーション内容
+# 【バリデーション内容】
+# presence（空かどうか）
+# length（文字数）
+# numericality（数値かどうか）
+# format（正しい形式かどうか）
+# uniqueness（一意かどうか）
+validates :title, presence: true
+validates :user_id, presence: true
 end
