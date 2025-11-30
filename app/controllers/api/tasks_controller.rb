19096a8027e7e9508e5453226709d364831a23be
hiromu terashima <hiromu.terashima@epkotsoftware.co.jp>
2025-11-30 11:26:50 +0900

    Rdoc追加

--- app/controllers/api/tasks_controller.rb
+++ app/controllers/api/tasks_controller.rb
@@ -1,5 +1,10 @@
 class Api::TasksController < ApplicationController
   protect_from_forgery with: :null_session
+
+  # タスクを全件取得する
+  # タスクが存在しない場合はメッセージを返す
+  # @return [Array<Task>] タスク情報一覧
+  # @return [Hash] タスクが存在しない場合 { status: "OK", result: "タスクはありません" }
   def index
     tasks = Task.all
     if tasks.empty?
@@ -9,6 +14,10 @@ class Api::TasksController < ApplicationController
     render json: { status: "OK", result: tasks }, status: :ok
   end
 
+  # IDを指定してタスクを取得する
+  # そのIDに対応するタスクが存在しない場合はメッセージを返す
+  # @return [Task] タスク情報
+  # @return [Hash] タスクが存在しない場合 { status: "OK", result: "{ID}そのタスクはありません" }
   def show
     task = Task.find_by(id: params[:id])
     id =  params[:id]
@@ -19,11 +28,19 @@ class Api::TasksController < ApplicationController
     render json: { status: "OK", result: task }, status: :ok
   end
 
+  # 受け取った値から新規タスクを作成する
+  # @param task_params [ActionController::Parameters] タスク名、説明、期日を含んだパラメータ
+  # @return [Hash] 作成したタスク情報
   def create
     task = Task.create(task_params)
     render json: { status: "OK", result: task }, status: :ok
   end
 
+  # IDで指定したタスクの情報を更新する
+  # そのIDに対応するタスクが存在しない場合はメッセージを返す
+  # @param task_params [ActionController::Parameters] タスク名、説明、期日を含んだパラメータ
+  # @return [Hash] 更新したタスク情報
+  # @return [Hash] タスクが存在しない場合 { status: "OK", result: "{ID}更新するタスクはありません" }
   def update
     task = Task.find_by(id: params[:id])
     if task.nil?
@@ -34,6 +51,11 @@ class Api::TasksController < ApplicationController
     render json: { status: "OK", result: task }, status: :ok
   end
 
+  # IDで指定したタスクを削除する
+  # そのIDに対応するタスクが存在しない場合はメッセージを返す
+  # @param task_params [ActionController::Parameters] タスク名、説明、期日を含んだパラメータ
+  # @return [Hash] 削除したタスク情報 { status: "OK", result: Task }
+  # @return [Hash] タスクが存在しない場合 { status: "OK", result: "{ID}削除するタスクはありません" }
   def destroy
     task = Task.find_by(id: params[:id])
     if task.nil?
@@ -44,14 +66,17 @@ class Api::TasksController < ApplicationController
     render json: { status: "OK", result: task }, status: :ok
   end
 
+  # 最も新しいタスクを削除する
+  # @return [Hash] 削除したタスク情報 { status: "OK", result: Task }
+  # @return [Hash] タスクが存在しない場合 { status: "OK", result: "タスクが1つもありません" }
   def delete_recently_task
     task = Task.order(created_at: :desc).first
     if task.nil?
       render json: { status: "OK", result: "タスクが1つもありません" }, status: :ok
       return
     end
-    result = task.destroy
-    render json: result, status: :ok
+    task.destroy
+    render json: { status: "OK", result: task }, status: :ok
   end
 
   private
