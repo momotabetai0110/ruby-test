0f8b87a69b1092afc81709c2fbd8334b36c7df76
hiromu terashima <hiromu.terashima@epkotsoftware.co.jp>
2025-12-02 00:46:13 +0900

    バリデーション再追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -6,6 +6,29 @@ class Task < ApplicationRecord
 # numericality（数値かどうか）
 # format（正しい形式かどうか）
 # uniqueness（一意かどうか）
-validates :title, presence: true
+
+# ユーザーID 必須
 validates :user_id, presence: true
+
+# タイトル 必須かつ20文字まで
+validates :title,
+          presence: true,
+          length: { maximum: 20 }
+
+# 説明 100文字まで nil許容
+validates :description,
+          length: { maximum: 100 },
+          allow_nil: true
+
+# 期限 今日以降の日付
+validate :check_due_date
+
+private
+
+def check_due_date
+  return if due_date.nil?
+  if due_date.to_date < Date.current
+    errors.add(:due_date, "に過去を設定することはできません")
+  end
+end
 end
